0f55d282a126f63b256e255772d77f51e9251bd3
RIYAS KHAN <riyastech001@gmail.com>
2026-08-01 17:33:01 +0530

    Simplify SQL query for unique countries

--- scripts/scripts/02_dimensions_exploration.sql
+++ scripts/scripts/02_dimensions_exploration.sql
@@ -13,9 +13,7 @@ SQL Functions Used:
 */
 
 -- Retrieve a list of unique countries from which customers originate
-SELECT DISTINCT 
-    country 
-FROM gold.dim_customers
+SELECT DISTINCT country FROM gold.dim_customers
 ORDER BY country;
 
 -- Retrieve a list of unique categories, subcategories, and products
